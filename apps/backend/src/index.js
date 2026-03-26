429a222a14cb1ac001a494151d862afd6ba3c72c
arbaz khan <itsarbaz51@gmail.com>
2026-03-26 13:39:13 +0530

    fix

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -1,14 +1,11 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "./.env" }); 
+dotenv.config({ path: "./.env" });
 import Prisma from "./db/db.js";
 import app from "./app.js";
 import { envConfig } from "./config/env.config.js";
-import { startCronJobs } from "./cron/index.js";
 
 (async function main() {
   try {
-    console.log("Cron starting...");
-    startCronJobs();
     console.log("Connecting to database...");
     await Prisma.$connect();
     console.log("✅ Database connected");
